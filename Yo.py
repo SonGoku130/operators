193724796f753fd25f6caf9d56a4010c84681878
SonGoku130 <aaryan2221t@gmail.com>
2026-06-22 17:08:18 +0530

    Add files via upload

---
+++ Yo.py
@@ -0,0 +1,24 @@
+a = 10
+b = 12
+c = 12
+
+print(a != b) #Here != means "Not equal to"
+print(b == c) #Here == means "Equal too"
+
+#Printing press
+print( "a = ", a, "b = ", b, "c = ", c )
+print("a != b = ", a != b) #True
+print("b == c = ", b == c) #True
+print("---")
+
+#String comparision cuz why not 
+a = "Dragon"
+b = "Shaft legends"
+
+if a != b:
+    print(a, "and", b, 'are different')
+
+print("a = ", a, "b = ", b)
+print("a != b = ", a != b)
+
+
